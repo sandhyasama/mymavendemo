3878e4d920158fc480a9cf207515fe84a138af19
Santhosh <santhoshreddy.soma@gmail.com>
2019-05-22 22:28:51 -0400

    adding maven files

---
+++ mymavendemo/src/main/java/com/practicetesting/starter/Starter.java
@@ -0,0 +1,11 @@
+package com.practicetesting.starter;
+
+public class Starter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}
